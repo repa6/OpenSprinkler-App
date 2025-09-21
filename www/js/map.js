f55a8457255df545b11e69e80802eb23bf0cd323
repa6 <repayadam@gmail.com>
2025-09-21 22:36:29 +0200

    edited google maps api key

--- www/js/map.js
+++ www/js/map.js
@@ -15,7 +15,7 @@
 
 // Create the script tag, set the appropriate attributes
 var script = document.createElement( "script" );
-script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDaT_HTZwFojXmvYIhwWudK00vFXzMmOKc&libraries=places&callback=initMap";
+script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDg1hJbL3c58gLQtX_3EBYSYU0cj-1RHII&libraries=places&callback=initMap";
 script.async = true;
 
 // Attach your callback function to the `window` object
